a9430dd4009ce4ec0b19eea6078fb1aa68618a47
ok-coder1 <codershub.code@gmail.com>
2025-08-26 23:35:35 +0530

    feat: a buncha commands
    Signed-off-by: ok-coder1 <codershub.code@gmail.com>

--- terminal/terminal.js
+++ terminal/terminal.js
@@ -1,5 +1,5 @@
 // Set variables and functions
-const currentDate = new Date();
+var currentDate = new Date();
 const rebootDate = new Date(currentDate.setSeconds(currentDate.getSeconds() + 1));
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
@@ -33,9 +33,123 @@ const commands = {
             location.reload(true);
         }, 5000);
     },
+    fastfetch()
+    {
+        var fastfetch_ascii = `        _,met$$$$$gg.
+     ,g$$$$$$$$$$$$$$$P.
+   ,g$$P""       """Y$$.".     guest@okcoder1
+  ,$$P'              \`$$$.     -------------
+',$$P       ,ggs.     \`$$b:    OS: Debian GNU/Linux 13 (trixie) x86_64
+\`d$$'     ,$P"'   .    $$$     Host: KVM/QEMU Standard PC (i440FX + PIIX, 1996) (pc-i440fx-9.2)
+ $$P      d$'     ,    $$P     Kernel: Linux 6.12.41+deb13-amd64
+ $$:      $$.   -    ,d$$'     Uptime: 1 day, 1 hour, 42 mins
+ $$;      Y$b._   _,d$P'       Packages: 4372 (dpkg), 47 (nix-default)
+ Y$$.    \`.\`"Y$$$$P"'          Shell: bash 5.2.37
+ \`$$b      "-.__               Cursor: Adwaita
+  \`Y$$b                        Terminal: /dev/pts/13
+   \`Y$$.                       CPU: AMD EPYC 9454P 48-Core (96) @ 2.75 GHz
+     \`$$b.                     GPU: Unknown Device 1111 (VGA compatible)
+       \`Y$$b.                  Memory: 165.29 GiB / 235.19 GiB (70%)
+         \`"Y$b._               Swap: 79.22 GiB / 470.39 GiB (17%)
+             \`""""             Disk (/): 2.52 TiB / 2.98 TiB (85%) - ext4
+                               Local IP (ens18): 37.27.51.34/32
+                               Locale: en_US.UTF-8`;
+        term.echo(rainbow(fastfetch_ascii));
+    },
+    ls()
+    {
+        term.echo(`README.md        `);
+    },
+    dir()
+    {
+        term.echo(`README.md`);
+    },
+    whoami()
+    {
+        term.echo(`guest`);
+    },
+    pwd()
+    {
+        term.echo(`/home/guest`)
+    },
+    async sha1sum(...args)
+    {
+        const input = args.join(' ');
+        const getSHA1Hash = async (input) => {
+            const textAsBuffer = new TextEncoder().encode(input);
+            const hashBuffer = await window.crypto.subtle.digest("SHA-1", textAsBuffer);
+            const hashArray = Array.from(new Uint8Array(hashBuffer));
+            const hash = hashArray
+              .map((item) => item.toString(16).padStart(2, "0"))
+              .join("");
+            return hash;
+        };
+        const hashed = await getSHA1Hash(input);
+        term.echo(`${hashed} ${input}`);
+    },
+    async sha256sum(...args)
+    {
+        const input = args.join(' ');
+        const getSHA256Hash = async (input) => {
+            const textAsBuffer = new TextEncoder().encode(input);
+            const hashBuffer = await window.crypto.subtle.digest("SHA-256", textAsBuffer);
+            const hashArray = Array.from(new Uint8Array(hashBuffer));
+            const hash = hashArray
+              .map((item) => item.toString(16).padStart(2, "0"))
+              .join("");
+            return hash;
+        };
+        const hashed = await getSHA256Hash(input);
+        term.echo(`${hashed} ${input}`);
+    },
+    async sha384sum(...args)
+    {
+        const input = args.join(' ');
+        const getSHA384Hash = async (input) => {
+            const textAsBuffer = new TextEncoder().encode(input);
+            const hashBuffer = await window.crypto.subtle.digest("SHA-384", textAsBuffer);
+            const hashArray = Array.from(new Uint8Array(hashBuffer));
+            const hash = hashArray
+              .map((item) => item.toString(16).padStart(2, "0"))
+              .join("");
+            return hash;
+        };
+        const hashed = await getSHA384Hash(input);
+        term.echo(`${hashed} ${input}`);
+    },
+    async sha512sum(...args)
+    {
+        const input = args.join(' ');
+        const getSHA512Hash = async (input) => {
+            const textAsBuffer = new TextEncoder().encode(input);
+            const hashBuffer = await window.crypto.subtle.digest("SHA-512", textAsBuffer);
+            const hashArray = Array.from(new Uint8Array(hashBuffer));
+            const hash = hashArray
+              .map((item) => item.toString(16).padStart(2, "0"))
+              .join("");
+            return hash;
+        };
+        const hashed = await getSHA512Hash(input);
+        term.echo(`${hashed} ${input}`);
+    },
+    cat(...args)
+    {
+        if (args == "README.md")
+        {
+            term.echo(`Hi there! I'm <b><green>okcoder1</green></b> as you may have seen above.\nI'm a <b>developer</b>. That's it?\nI mainly use <b><yellow>Python</yellow></b> (even though this website is in <b><i>JavaScript</i></b>) and <b>macOS</b> is my daily driver.\nCheckout (<i>git</i> it?) my (un)cool projects by typing '<b>github</b>'!\nType '<b>help</b>' to see more commands.`);
+        }
+        else if (args == "")
+        {
+            term.echo(`cat: No file specified`);
+        }
+        else
+        {
+            term.echo(`cat: <b>${args}</b>: No such file or directory`)
+        }
+    },
     help()
     {
-        term.echo(`Commands that are available: <b>${help}<b>`);
+        term.echo(`Commands: <b>${help}<b>`);
     },
     echo(...args)
     {
@@ -48,12 +162,12 @@ const commands = {
     },
     hackclub_slack()
     {
-        term.echo(`(Hack Club) This is where I usually <yellow><b>Slack</yellow></b> off.`);
+        term.echo(`(Hack Club) This is where I usually <yellow><b>Slack</yellow></b> off.\n<b><red>WARNING</red>: You need to be a member of Hack Club to view this (you need to be <i>18</i> or under to join).</b>`);
         window.open('https://hackclub.slack.com/team/U08F95SST0V', '_blank');
     },
     twitch()
     {
-        term.echo(`<purple><b>Twitch</purple></b>? You're gonna be <b>twitching your eye</b> when you see my streams (which I never do often)!`);
+        term.echo(`<purple><b>Twitch</purple></b>? My streams (which I never do) are bad. Just a warning.`);
         window.open('https://www.twitch.tv/ok_coder1', '_blank');
     },
     github()
@@ -88,11 +202,23 @@ const formatter = new Intl.ListFormat('en',
                                             type: 'conjunction',
                                         });
 const command_list = ['clear'].concat(Object.keys(commands));
-const help = formatter.format(command_list);
+const formatted_list = command_list.map(cmd => {
+    return `<white class="command-click-help">${cmd}</white>`;
+});
+const help = formatter.format(formatted_list);
+term.on('click', '.command-click-help', function() {
+    const command_click = $(this).text();
+    term.exec(command_click);
+ });
 
 // Colour Formatting
 function rand(max) {
     return Math.floor(Math.random() * (max + 1));
+}
+function hex(color) {
+    return '#' + [color.red, color.green, color.blue].map(n => {
+        return n.toString(16).padStart(2, '0');
+    }).join('');
 }
 function rainbow(string) {
     return lolcat.rainbow(function(char, color)
@@ -100,12 +226,6 @@ function rainbow(string) {
         char = $.terminal.escape_brackets(char);
         return `[[;${hex(color)};]${char}]`;
     }, string).join('\n');
-}
-
-function hex(color) {
-    return '#' + [color.red, color.green, color.blue].map(n => {
-        return n.toString(16).padStart(2, '0');
-    }).join('');
 }
 
 // Greetings
@@ -128,22 +248,32 @@ async function ready()
 {
     term.clear();
     term.set_prompt('').pause(true);
+    var currentDate = new Date();
     term.echo(() => `(<b>INFO</b>) [${currentDate}] Starting up...`).addClass('startup-log');
+    var currentDate = new Date();
     await sleep(1000);
     term.echo(() => `(<b><green>SUCCESS</green></b>) [${currentDate}] Successfully loaded all modules!`).addClass('startup-log');
+    var currentDate = new Date();
     await sleep(500);
     term.echo(() => `(<b>INFO</b>) [${currentDate}] Loading jQuery...`).addClass('startup-log');
+    var currentDate = new Date();
     await sleep(1000);
     term.echo(() => `(<b>INFO</b>) [${currentDate}] Loading jQuery Terminal...`).addClass('startup-log');
+    var currentDate = new Date();
     await sleep(1500);
     term.echo(`(<b><green>SUCCESS</green></b>) [${currentDate}] Successfully loaded jQuery!`).addClass('startup-log');
+    var currentDate = new Date();
     term.echo(`(<b>INFO</b>) [${currentDate}] Starting up services...`).addClass('startup-log');
+    var currentDate = new Date();
     await sleep(750);
     term.echo(`(<b><green>SUCCESS</green></b>) [${currentDate}] Successfully started services!`).addClass('startup-log');
+    var currentDate = new Date();
     await sleep(250);
     term.echo(`(<b>INFO</b>) [${currentDate}] Loading terminal...`).addClass('startup-log');
+    var currentDate = new Date();
     await sleep(1000);
     term.echo(`(<b><green>SUCCESS</green></b>) [${currentDate}] Successfully loaded terminal!`).addClass('startup-log');
+    var currentDate = new Date();
     await sleep(500);
     term.clear();
     term.echo(rainbow(render('okcoder1')), { ansi: true });
